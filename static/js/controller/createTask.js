4b601993d914625544fbc0a69e5b5e8ad8dbd6f7
Fernando Jose <delgadof@seattleu.edu>
2016-07-29 17:33:40 -0400

    Date and time working

--- static/js/controller/createTask.js
+++ static/js/controller/createTask.js
@@ -1,4 +1,4 @@
-mainApp.controller('createTaskController', function($scope, $location, $http) {
+mainApp.controller('createTaskController', function($scope, $location, $http, $filter) {
     console.log("create task Controller");
 
     $scope.formData = {
@@ -27,81 +27,20 @@ mainApp.controller('createTaskController', function($scope, $location, $http) {
         });
     }
 
-    $scope.today = function() {
-        $scope.dt = new Date();
-    };
-    $scope.today();
-
-    $scope.clear = function () {
-        $scope.dt = null;
-    };
-
-    $scope.toggleMin = function() {
-        $scope.minDate = $scope.minDate ? null : new Date();
-    };
-    $scope.toggleMin();
-
     $scope.open = function($event) {
         $event.preventDefault();
         $event.stopPropagation();
-
         $scope.opened = true;
     };
 
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-    };
-
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-
-    $scope.dynamicPopover = {
+    $scope.startPopover = {
         templateUrl: 'templates/includes/startTime.html'
     };
 
-    $scope.mytime = new Date();
-
-    $scope.hstep = 1;
-    $scope.mstep = 15;
-
-    $scope.options = {
-        hstep: [1, 2, 3],
-        mstep: [1, 5, 10, 15, 25, 30]
+    $scope.endPopover = {
+        templateUrl: 'templates/includes/endTime.html'
     };
 
-    $scope.ismeridian = true;
-    $scope.toggleMode = function() {
-        $scope.ismeridian = ! $scope.ismeridian;
-    };
-
-    $scope.update = function() {
-        var d = new Date();
-        d.setHours( 14 );
-        d.setMinutes( 0 );
-        $scope.mytime = d;
-    };
-
-    $scope.changed = function () {
-        console.log('Time changed to: ' + $scope.mytime);
-    };
-
-    $scope.clear = function() {
-        $scope.mytime = null;
-    };
-
-    $(function () {
-        $('#check').webuiPopover({trigger:'manual', placement:'bottom', type:'html', arrow:false});
-        $('#check').on("click", function (event) {
-            var self = $('#check');
-            if (self.is(':checked')) {
-                self.webuiPopover('show');
-            }
-            else {
-                self.webuiPopover('hide');
-            }
-        });
-    })
 
     $('.form-active-basic').on('focus blur', function() {
         $('#basic').toggleClass("form-active-blue");
@@ -124,12 +63,21 @@ mainApp.controller('createTaskController', function($scope, $location, $http) {
         var data = {}
         data.owner_pk = 1;
 
-        fields = ['title', 'video', 'cagetory', 'help_text', 'recurring', 
-            'recurring_weekly', 'recurring_daily']
+        fields = ['title', 'video', 'cagetory', 'help_text', 'recurring',
+        'recurring_weekly', 'recurring_daily', 'date', 'start_time', 'end_time']
 
         for (i in fields) {
             if (fields[i] in $scope.formData) {
                 data[fields[i]] = $scope.formData[fields[i]]
+            }
+            if (fields[i] == 'date') {
+                $scope.formData.date = $filter('date')($scope.formData.date, 'yyyy-MM-dd');
+            }
+            if (fields[i] == 'start_time') {
+                $scope.formData.start_time = $filter('date')($scope.formData.start_time, 'HH:mm');
+            }
+            if (fields[i] == 'end_time') {
+                $scope.formData.end_time = $filter('date')($scope.formData.end_time, 'HH:mm');
             }
         }
 
@@ -152,7 +100,7 @@ mainApp.controller('createTaskController', function($scope, $location, $http) {
                 steps.push(s)
             }
             data.steps = steps.join(":::")
-        } 
+        }
 
         var req = {
             url: "http://iamready.herokuapp.com/events/mastertask/create/",
@@ -164,9 +112,8 @@ mainApp.controller('createTaskController', function($scope, $location, $http) {
             console.log("Created!")
         })
         .error(function(data){
-            
         })
     }
 
-    
+
 });
