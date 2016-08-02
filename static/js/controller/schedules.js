570a077cc3c29fe69d12945534421adae3455e09
Fernando Jose <delgadof@seattleu.edu>
2016-08-02 16:57:03 -0700

    Some punch list fixes

--- static/js/controller/schedules.js
+++ static/js/controller/schedules.js
@@ -1,6 +1,19 @@
 mainApp.controller('schedulesController', function($scope, $http) {
     console.log("schedules Controller");
 
+    var ureq = {
+        url: "http://iamready.herokuapp.com/users/user/all/",
+        data: {
+            pk: 1,
+            mode: "simple"
+        },
+        method: "POST"
+    }
+
+    $http(ureq).success(function(data){
+        $scope.users = data
+    })
+
     $scope.today = function() {
         $scope.dt = new Date();
     };
